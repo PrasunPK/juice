4a2c7e05425bd21887479053d040bb2521fba0a2
PrasunPK <prasun_1993@rediffmail.com>
2016-02-08 10:52:43 +0530

    Basic chart is being done

--- graph.js
+++ graph.js
@@ -1,11 +1,23 @@
-// d3.json("juice_orders", function(data) {
-//     console.log(data.length);
-// });
-var data = [4, 8, 15, 16, 23, 42];
+d3.json("juice_orders", function(data) {
+    var quantity = {};
+    for(var i = 0; i < data.length; i++){
+        var drink = data[i];
+        if(quantity[drink.drinkName] === undefined)
+            quantity[drink.drinkName] = drink.quantity;
+        else
+            quantity[drink.drinkName] += drink.quantity;
+    }
 
-d3.select(".chart")
-  .selectAll("div")
-    .data(data)
-  .enter().append("div")
-    .style("width", function(d) { return d * 10 + "px"; })
-    .text(function(d) { console.log(d);return d; });
+    var scale = d3.scale.linear()
+                        .domain([0,1000])
+                        .range([0,600]);
+
+    var drinkNames = Object.keys(quantity);
+
+    d3.select(".chart")
+    .selectAll("div")
+    .data(drinkNames)
+    .enter()
+    .append("div")
+    .style("width", function(d) { return scale(quantity[d])+'px'; })
+});
